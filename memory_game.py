d896a00de1589004dd97bb157b54be19d8cd24f4
JuanMa <jmgro1908@gmail.com>
2026-05-08 20:54:16 -0600

    Centre los numeros de la esquina izquierda

--- memory_game.py
+++ memory_game.py
@@ -56,7 +56,7 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x + 25, y + 8)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
